01498d5ce3bb6b87f2c636b31c34c6ebd2ec584b
james Zhang <ztj.james@gmail.com>
2024-06-11 11:06:32 +0800

    working stream

---
+++ central_host.py
@@ -0,0 +1,30 @@
+print("peers.html is a PeerJS video chat interface that allows users to join a room, select a user to call, and make a video call.")
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+peers = {}
+
+@app.route('/')
+def index():
+    return render_template('peers.html')
+
+@app.route('/join', methods=['POST'])
+def join_room():
+    room_id = request.form['roomId']
+    peer_id = request.form['peerId']
+    peers[room_id] = peers.get(room_id, []) + [peer_id]
+    return 'Joined room: ' + room_id
+
+@app.route('/get_peers', methods=['POST'])
+def get_peers():
+    room_id = request.form['roomId']
+    return {'peers': peers.get(room_id, [])}
+
+
+if __name__ == '__main__':
+    app.run(debug=False, 
+        host="0.0.0.0", 
+        port="5000",
+        ssl_context='adhoc'
+     )
